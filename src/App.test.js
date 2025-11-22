517d2bcd24d3b6614205f3a791c5691cb2f89593
Jayawanth Rayakota Harnath <67304244+JayawanthRh@users.noreply.github.com>
2025-11-22 15:53:21 -0500

    Fix App.test.js by mocking components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+
+jest.mock('./components/projects/Projects', () => () => <div>Projects</div>);
+jest.mock('./components/skills/Skills', () => () => <div>Skills</div>);
+jest.mock('./components/contact/Contact', () => () => <div>Contact</div>);
+jest.mock('./components/experience/Experience', () => () => <div>MockedExperience</div>);
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Experience section', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/MockedExperience/i);
   expect(linkElement).toBeInTheDocument();
 });
